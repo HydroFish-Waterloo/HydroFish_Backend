1b5057518065ee23d8712d917bc16b6ac78d9266
marklhj-lee <marklhj.lee@mail.utoronto.ca>
2024-03-03 12:32:08 -0500

    Clean up unused file and uncomment code

--- backend_app/views.py
+++ backend_app/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-# from rest_framework import status
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-# from .models import WaterIntake
-# from .serializers import WaterIntakeSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import WaterIntake
+from .serializers import WaterIntakeSerializer
 
-# class WaterIntakeCreateAPIView(APIView):
-#     def post(self, request, *args, **kwargs):
-#         # Set user_id to 0 by default
-#         request.data['user_id'] = 0
+class WaterIntakeCreateAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        # Set user_id to 0 by default
+        request.data['user_id'] = 0
         
-#         serializer = WaterIntakeSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = WaterIntakeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
