8851d22be39be579c2bbff0e1ba0c9b78afeed24
Arthur_linux <iamleary@live.com>
2024-03-05 09:43:04 -0500

    web, tested login, add register.

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,10 +2,12 @@ from django.urls import re_path, path
 from django.contrib.auth.views import LoginView
 
 from . import views
-from .views import LoginAPIView
+## import from my code
+from .views import LoginAPIView, RegisterAPIView
 
 #app_name = 'users'
 
 urlpatterns = [
     path('api/login/', LoginAPIView.as_view(), name='api_login'), #api for mobile app. [mar 4]
+    path('api/register/', RegisterAPIView.as_view(), name='api_register'), #api for mobile app. [mar 5]
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.http import JsonResponse
 from django.urls import reverse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
@@ -8,6 +9,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
 
@@ -25,7 +27,21 @@ class LoginAPIView(APIView):
             return Response({"error": "Invalid Credentials"}, status=status.HTTP_400_BAD_REQUEST)
         
 
+
 # Endpoint: /api/register
 class RegisterAPIView(APIView):
+    permission_classes = [AllowAny]  # Allow unauthenticated users to access this view for registration
+    
     def post(self, request, *args, **kwargs):
-        return 0
+        username = request.data.get("username")
+        password = request.data.get("password")
+        
+        form = UserCreationForm(data={'username': username, 'password': password})
+        
+        if form.is_valid():
+            user = form.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+
