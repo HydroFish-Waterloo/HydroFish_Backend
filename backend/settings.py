0c9fb08e64f8ee2a82995476d7139aa452f66bfa
Xiaoyu Zou <x47zou@uwaterloo.ca>
2024-03-13 16:46:18 -0400

    Change Login and Register API to Return UserInfo Change settings for connecting server in local environment

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4fii@*tvz5gqf8jw3s00j$n-!k%79a%9@b72r7rgm@!bk$$)@g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1']
 
 
 # Application definition
